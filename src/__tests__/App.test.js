6d7f5a460b757f77ee2f6b00da1bbb8fce39d960
Abraham <cedenoabraham@gmail.com>
2019-08-09 20:50:00 -0300

    setup jest and enzyme

---
+++ src/__tests__/App.test.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { shallow } from 'enzyme';
+import App from '../App';
+
+describe('Examining the syntax of Jest tests', () => {
+    it('sums numbers', () => {
+        expect(1 + 2).toEqual(3);
+        expect(2 + 2).toEqual(4);
+    });
+});
+
+describe('First React component test with Enzyme', () => {
+    it('renders without crashing', () => {
+        const Wrapper = shallow(<App />);
+        expect(Wrapper).toMatchSnapshot()
+    });
+});
