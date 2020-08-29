c669d6b163292aea6c70f46e5e143db90bf04055
Abraham <cedenoabraham@gmail.com>
2020-08-29 03:21:53 -0300

    clip path border workaround
    svg background for items
    
    responsiveness

--- src/components/PokeImage.js
+++ src/components/PokeImage.js
@@ -3,32 +3,37 @@ import PropTypes from "prop-types";
 import styled from '@emotion/styled';
 
 const Div = styled.div `
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    font-size: 0.5rem;
-    color: black;
+    position: relative;
+    font-size: 1.5rem;
+    font-weight: bold;
     text-transform: uppercase;
+    color: white;
     &:hover {
         cursor: pointer;
     }
     figure {
+        padding: 1rem;
+        position: relative;
         margin: 0;
         img {
-            width: 75px;
-            height: 75px;
+            position: absolute;
+            right: -.5rem;
+            top: -2.5rem;
+            width: 125px;
+            height: 125px;
         }
     }
 `;
 
 function PokeImage({pokemon, handleClick}){
-    if(!pokemon.sprites) return <Div></Div>;
+    const id = pokemon.id;
+    if(!id) return null;
     return (
         <Div>
             <figure>
                 <img
                     data-testid="image"
-                    src={pokemon.sprites.front_default}
+                    src={`https://assets.pokemon.com/assets/cms2/img/pokedex/full/${id.toString().padStart(3, "0")}.png`}
                     alt={pokemon.name}
                     onClick={ () => handleClick(pokemon.name)}
                 />
--- src/components/Pokelist/PokeList.js
+++ src/components/Pokelist/PokeList.js
@@ -3,7 +3,7 @@ import { withRouter } from "react-router-dom";
 import styled from '@emotion/styled';
 import {useSpring, animated} from 'react-spring';
 
-const PokeItem = lazy(() => import('./PokeItem'));
+const PokeItem = lazy(() => import('../PokeItem'));
 
 const renderLoader = () => <p>Loading</p>;
 
@@ -12,12 +12,35 @@ const Section = styled(animated.section) `
     justify-content: space-around;
     align-items: center;
     flex-wrap: wrap;
-    background-color: transparent;
-    padding-top: 85px;
-    width: 408px;
-    height: 385px;
+    background-color: white;
+    padding-top: 0px;
+    width: 100vw;
+    height: 100vh;
     margin: 0 auto;
+    border: 6px solid black;
     overflow-y: scroll;
+`;
+
+const Header = styled.div `
+    position: relative;
+    top: 0;
+    width: 100%;
+    background: red;
+    height: 250px;
+    clip-path: polygon(50% 0%, 100% 0, 100% 26%, 100% 64%, 65% 64%, 38% 100%, 0 100%, 0% 43%, 0 0);
+    z-index: 2;
+    overflow: hidden;
+`;
+
+const Wrap = styled.span `
+    position: relative;
+    top: 0;
+    margin: 0 0 3rem 0;
+    width: 100%;
+    background: black;
+    height: 256px;
+    clip-path: polygon(50% 0%, 100% 0, 100% 26%, 100% 64%, 65% 64%, 38% 100%, 0 100%, 0% 43%, 0 0);
+    border-bottom: 6px solid black;
 `;
 
 const PokeList = ({history}) => {
@@ -31,6 +54,9 @@ const PokeList = ({history}) => {
 
     return (
         <Section style={props} data-testid="list-items">
+            <Wrap>
+                <Header />
+            </Wrap>
             {[...Array(151)].map( (e,id) => (
                 <Suspense fallback={renderLoader()}>
                     <PokeItem id={(id + 1).toString()} key={id.toString()} handleClick={handleClick}/>
